abf532420791d77bdc957697643c80a0a550ff30
Subhath <hasirasubath@gmail.com>
2023-03-09 22:55:34 +0530

    create crud

--- backend/routes/ToDoRoute.js
+++ backend/routes/ToDoRoute.js
@@ -1,10 +1,13 @@
-const {Router} = require("express")
+const {Router} = require("express");
+const { getToDo, saveToDo, updateToDo, deleteToDo } = require("../controllers/ToDoController");
 
 
 const router = Router()
 
-router.get('/',(req, res) => {
-    res.json({message: "ado kohomda..."})
-})
+router.get('/', getToDo)
+router.post('/save', saveToDo)
+router.post('/update', updateToDo)
+router.post('/delete', deleteToDo)
+
 
 module.exports = router;
