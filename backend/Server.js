cbd82ed4c76555f19d26932e452ab84c3ff8fb18
Subhath <hasirasubath@gmail.com>
2023-03-31 13:24:24 +0530

    fix:port

--- backend/Server.js
+++ backend/Server.js
@@ -7,7 +7,7 @@ const routes = require('./routes/ToDoRoute')
 require('dotenv').config()
 
 const app = express()
-const PORT = process.env.port || 3000
+const PORT = process.env.port || 4000
 
 
 app.use(express.json())
